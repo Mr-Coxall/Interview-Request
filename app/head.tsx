a8e67575c1eddf666fb32c5a248446d31a30d452
Patrick Coxall <mr.coxall@mths.ca>
2023-05-01 19:29:15 +0000

    add meta data

--- app/head.tsx
+++ app/head.tsx
@@ -1,12 +1,30 @@
 export default function Head() {
-	return (
-		<>
-			<title>Linktree Redesign Theme</title>
-			<meta
-				content="width=device-width, initial-scale=1"
-				name="viewport"
-			/>
-			<link rel="icon" href="/favicon.ico" />
-		</>
-	)
+  return (
+    <>
+      <title>Interview Request</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="The Interview Request App" />
+      <meta name="interview" content="request" />
+      <meta name="author" content="Mr. Coxall" />
+      <link rel="manifest" href="site.webmanifest" />
+      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="favicon-16x16.png"
+      />
+    </>
+  );
 }
